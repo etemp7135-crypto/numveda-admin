2aeb38ca67967ca5fa330c5d4b55d3537270d77d
Niravgithub2005 <niravlakhani2125@gmail.com>
2026-08-21 02:07:25 +0530

    feat: replace env variable with hardcoded test request for Meta Graph API integration

--- test-meta.js
+++ test-meta.js
@@ -1,2 +1,7 @@
-const AD_ACCOUNT_ID = process.env.META_AD_ACCOUNT_ID;
-console.log("Checking AD_ACCOUNT_ID format:", AD_ACCOUNT_ID);
+const token = 'EAAWJZBoLG2psBSSJR8q4ivKDLWpm8H8ZBzbiwoHtARsgYKyT6yeZA9mAGemfG0kl4qUcNBc6JyZCXo0rPnx4c2W6CWbq3cPh3OaRfEVamenCOIZByolwDNXsi2bZBn1gZAXNvYwV7cFYeUFW9iZBKfGiPyvJouNMjYsNPAu1u9dKX3466eQ9YzB5NZApkMuoSGtATrNEgZCTbVRTlB1qq2V1VZCAuY0Fb3nXaRHTWY9p6x0';
+const accountId = '1074554091808150';
+
+fetch(`https://graph.facebook.com/v21.0/act_${accountId}?fields=id,name&access_token=${token}`)
+  .then(res => res.json())
+  .then(data => console.log('Result:', data))
+  .catch(err => console.error(err));
